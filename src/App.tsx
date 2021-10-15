3df7ab9b47846c76bb7ab107853ff8cd40ebf44f
NVTP <40886938+NVTP@users.noreply.github.com>
2021-10-15 21:23:26 +0700

    Initial Project

---
+++ src/App.tsx
@@ -0,0 +1,37 @@
+import { lazy, Suspense } from "react";
+import "./App.css";
+import { Provider } from "react-redux";
+import { store } from "./app/redux/store";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { ReactQueryDevtools } from "react-query/devtools";
+const client = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+});
+
+const IndexPage = lazy(() => import("./pages/index"));
+const MovieDetail = lazy(() => import("./pages/movie-detail"));
+
+function App() {
+  return (
+    <QueryClientProvider client={client}>
+      <ReactQueryDevtools />
+      <Provider store={store}>
+        <Suspense fallback={null}>
+          <Router>
+            <Switch>
+              <Route path="/" exact component={IndexPage} />
+              <Route path="/movie/:movieId" component={MovieDetail} />
+            </Switch>
+          </Router>
+        </Suspense>
+      </Provider>
+    </QueryClientProvider>
+  );
+}
+
+export default App;
